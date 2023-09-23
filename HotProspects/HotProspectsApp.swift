5902a06b4ad797741ef74d0bb5fea2d28be3bf4c
n1K1Ta00 <aleks.avers12@mail.ru>
2023-09-23 18:14:36 +0300

    add files

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Никита Мартьянов on 21.09.23.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
